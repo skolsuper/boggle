321e97e5886de915f1ad1295bc15ba131edd8114
James Keys <james@telenordigital.com>
2018-08-25 19:50:13 +0700

    Create a graph data structure for board

--- index.tsx
+++ index.tsx
@@ -13,6 +13,29 @@ import {
 const BOARD_HEIGHT = 4;
 const BOARD_WIDTH = 4;
 
+const range: (to: number) => number[] = R.range(0);
+
+const getCol = (index: number) => Math.floor(index / BOARD_HEIGHT);
+const getRow = (index: number) => R.modulo(index, BOARD_WIDTH);
+
+/**
+ * A map of the valid moves from each node.  Basically a graph.
+ * @example {
+ *  0: [1, 4, 5]
+ *  1: [0, 2, 4, 5, 6]
+ *  ...
+ * }
+ */
+const adjacencyMap = range(BOARD_WIDTH * BOARD_HEIGHT)
+    .reduce((acc: { [key: number]: number[] }, i) => {
+        acc[i] = range(BOARD_WIDTH * BOARD_HEIGHT).filter((j) => {
+            const distSq = Math.pow((getRow(i) - getRow(j)), 2)
+                + Math.pow((getCol(i) - getCol(j)), 2);
+            return distSq > 0 && distSq <= 2;
+        });
+        return acc;
+    }, {});
+
 const store = createStore(
     reducer,
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
