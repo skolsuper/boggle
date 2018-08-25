451be9ad32cbd4b083b20364fe0dec064de9ec7e
James Keys <james@telenordigital.com>
2018-08-25 11:46:58 +0700

    Basic scaffolding and boilerplate

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-export {};
+import { Action, createStore } from 'redux';
+
+class BoggleState {
+    private board?: string;
+}
+
+const store = createStore(reducer);
+
+export { store };
+
+function reducer(state: BoggleState = {}, action?: Action): object {
+    return state;
+}
