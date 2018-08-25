be453c70c6f1fc70460b76a10f2b7f0f73fb54e4
James Keys <james@telenordigital.com>
2018-08-25 17:39:49 +0700

    Some interaction

--- index.tsx
+++ index.tsx
@@ -1,38 +1,92 @@
 import R from 'ramda';
 import React from 'react';
-import { render } from 'react-dom'
-import { Action, createStore } from 'redux';
-import { Provider } from 'react-redux';
+import {render} from 'react-dom'
+import {createStore} from 'redux';
 
-class BoggleState {
-    private board?: string;
+const ACTIONS = {
+    SELECT_CELL: 'SELECT_CELL',
+    SET_BOARD: 'SET_BOARD',
+};
+
+const store = createStore(
+    reducer,
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+store.dispatch(setBoard('TAP*EAKSOBRSS*XD'));
+
+render(
+    (<div>
+        <input
+            onChange={(e) => store.dispatch(setBoard(e.target.value))}
+            type="text"
+            value={store.getState().board}
+        />
+        <Board width={4} height={4}/>
+        <div>{store.getState().selected.join()}</div>
+    </div>),
+    document.getElementById('root')
+);
+
+function reducer(state: IBoggleState = {
+    board: '****************',
+    selected: [],
+}, action: any): IBoggleState {
+    switch (action.type) {
+        case ACTIONS.SELECT_CELL:
+            return Object.assign({}, state, {
+                selected: [...state.selected, getLetterFromBoard(state.board, action.index)],
+            };
+        case ACTIONS.SET_BOARD:
+            return Object.assign({}, state, {
+                board: action.board,
+            };
+        default:
+            return state;
+    }
 }
 
-const store = createStore(reducer);
-
-function reducer(state: BoggleState = {}, action?: Action): object {
-    return state;
+interface IBoggleState {
+    board: string;
+    selected: string[];
 }
 
 function Board({ width, height }: { width: number, height: number }) {
     return (<table>
-        { R.map((i) => <Row row={i} width={width} height={height} />, R.range(0, height))}
+        <tbody>
+        { R.map((i) => <Row key={i} row={i} width={width} height={height} />, R.range(0, height))}
+        </tbody>
     </table>);
 }
 
 function Row({ row, width, height }: { row: number, width: number, height: number }) {
     return (<tr>
-        { R.map((col) => <Cell index={row * height + col} />, R.range(0, width))}
+        { R.map((col) => <Cell key={col} index={row * height + col} />, R.range(0, width))}
     </tr>);
 }
 
 function Cell({ index }: { index: number }) {
-    return <td>{index}</td>;
+    return (
+        <td onClick={() => store.dispatch(selectCell(index))}>
+        {getLetterFromBoard(store.getState().board, index)}
+        </td>
+    );
 }
 
-render(
-    <Provider store={store}>
-        <Board width={4} height={4}/>
-        </Provider>,
-    document.getElementById('root')
-);
+function setBoard(board: string) {
+    return {
+        board,
+        type: ACTIONS.SET_BOARD,
+    }
+}
+
+function selectCell(index: number) {
+    return {
+        index,
+        type: ACTIONS.SELECT_CELL,
+    };
+}
+
+function getLetterFromBoard(board: string, index: number): string {
+    return board[index];
+}
